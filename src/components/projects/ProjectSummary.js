2404256322bb905c65390b2972c79d9565224710
Jeff Edmondson <edmondso006@knights.gannon.edu>
2019-02-10 22:02:32 -0500

    Refactored short description

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
 const ProjectSummary = ({project}) => {
+  let shortDescription = project.content.slice(0, 200);
   return (
  
     <Card style={{ width: '36rem', marginTop: '1rem' }}>
@@ -11,7 +12,7 @@ const ProjectSummary = ({project}) => {
       <Card.Body>
         <Card.Subtitle className="mb-2 text-muted">{project.organizationName}</Card.Subtitle>
         <Card.Text>
-          {project.content.slice(0, 200).length >= 195 ? project.content.slice(0, 200) + '...' : project.content}
+          {shortDescription.length >= 195 ? shortDescription + '...' : project.content}
         </Card.Text>
         <Link to={'/project/' + project.id}  key={project.id} >
           <Button variant="primary">View Project!</Button>
