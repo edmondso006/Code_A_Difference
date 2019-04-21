486213ad4fc9ba9d5e704554228425a276e9fc65
Jeffrey Edmondson <edmondso006@knights.gannon.edu>
2019-04-21 19:59:16 -0400

    Added delete / confirmation

--- src/components/profile/profileProject.js
+++ src/components/profile/profileProject.js
@@ -4,65 +4,77 @@ import { deleteProject } from '../../store/actions/projectActions';
 import { Link } from 'react-router-dom';
 
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 class ProfileProject extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      deleteConfirm: false
+      deleteConfirm: false,
+      show: false
     }
   }
+
+  handleClose = () =>{
+    this.setState({ show: false });
+  }
+
+  handleShow = () => {
+    this.setState({ show: true });
+  }
+
 
   handleClick = (e) => {          
     {/* TODO: Alert are you sure you want to delete box */}
     e.preventDefault();
-    this.props.deleteProject(this.props.project)
+    this.props.deleteProject(this.props.project);
+    this.setState({ show: false })
   }
 
   render(){
     return (
+      <div>
+        <Card style={{ marginTop: '1rem' }}>
+          <Card.Header as="h5">{this.props.project.title}</Card.Header>
 
-      <Card style={{ marginTop: '1rem' }}>
-        <Card.Header as="h5">{this.props.project.title}</Card.Header>
+          <Card.Body>
+            <Card.Subtitle className="mb-2 text-muted">{this.props.project.category}</Card.Subtitle>
 
-        <Card.Body>
-          <Card.Subtitle className="mb-2 text-muted">{this.props.project.category}</Card.Subtitle>
+            <Card.Text>
+              Category: {this.props.project.category}
+              {this.props.project.createdAt.slice(0, 15)}
+            </Card.Text>
+          </Card.Body>
 
-          <Card.Text>
-            Category: {this.props.project.category}
-            {this.props.project.createdAt.slice(0, 15)}
-          </Card.Text>
-        </Card.Body>
+          <Card.Body>
+            <Button onClick={this.handleShow}>Delete</Button>
+            <Card.Link href="#">Edit</Card.Link>
+            <Link to={'/project/' + this.props.project.id} key={this.props.project.id} className="card-link" >
+              View
+            </Link>
+          </Card.Body>
+        </Card>
 
-        <Card.Body>
-          <Card.Link href="#">Delete</Card.Link>
-          <Card.Link href="#">Edit</Card.Link>
-          <Link to={'/project/' + this.props.project.id} key={this.props.project.id} className="card-link" >
-            View
-          </Link>
-        
-          
-        </Card.Body>
-        
-      </Card>
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Warning</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Are you sure you want to delete this project?</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              No
+            </Button>
+            <Button variant="primary" onClick={this.handleClick}>
+              Yes
+            </Button>
+          </Modal.Footer>
+        </Modal>
 
-      // <div className="card s12 m6 project-summary">
-      //   <div className="card-content grey-text text-darken-3">
-      //     <span className="card-title">{this.props.project.title}</span>
-      //     <div>Category: {this.props.project.category}</div>
-      //     <div>{this.props.project.createdAt.slice(0, 15)}</div>
-      //   </div>
-  
-      //   <div className="card-action grey lighten-4 grey-text">
-      //     <a href="#" onClick={this.handleClick} className="right-align">Delete</a>
-      //     {/* TODO: Alert are you sure you want to delete box */}
-      //     <a href="#" className="right-align">Edit</a>
-      //     <Link to={'/project/' + this.props.project.id} key={this.props.project.id} >
-      //       View
-      //     </Link>
-      //   </div>
-      // </div>
+      </div>
+      
+
     )
   }
   
